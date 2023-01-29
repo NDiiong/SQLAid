bed6dcdda8b2a6a3ab57ca85d1943883b54e1150
nb.duong <nb.duong@outlook.com>
2023-01-29 12:53:38 +0700

    Fixbuild SSMS 19

---
+++ src/SQLAid.Addin/Commands/TextEditor/Highlighter/HighlightCommand.cs
@@ -0,0 +1,43 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextManager.Interop;
+using SQLAid.Integration.DTE;
+using System;
+using Task = System.Threading.Tasks.Task;
+
+namespace SQLAid.Commands.TextEditor.Highlighter
+{
+    public static class HighlightCommand
+    {
+        private static WindowEvents _windowEvents;
+        private static IServiceProvider _serviceProvider;
+
+        public static async Task InitializeAsync(SqlAsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            _serviceProvider = package;
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            _windowEvents = dte.Events.WindowEvents;
+            _windowEvents.WindowCreated += _windowEvents_WindowCreated;
+        }
+
+        private static void _windowEvents_WindowCreated(Window Window)
+        {
+            try
+            {
+                var vsTextManager = _serviceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+                var componentModel = (IComponentModel)_serviceProvider.GetService(typeof(SComponentModel));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                throw;
+            }
+
+            //throw new NotImplementedException();
+        }
+    }
+}
--- src/SQLAid.Addin/SqlAidAsyncPackage.cs
+++ src/SQLAid.Addin/SqlAidAsyncPackage.cs
@@ -2,6 +2,7 @@
 using SQLAid.Commands.Events;
 using SQLAid.Commands.Grid;
 using SQLAid.Commands.TextEditor;
+using SQLAid.Commands.TextEditor.Highlighter;
 using SQLAid.Integration.DTE;
 using System;
 using System.Runtime.InteropServices;
@@ -36,6 +37,8 @@ namespace SQLAid
             await SqlJoinLinesCommand.InitializeAsync(this).ConfigureAwait(false);
             await SqlResultGridAsInsertScriptCommand.InitializeAsync(this).ConfigureAwait(false);
             await SqlPasteAsCsvCommand.InitializeAsync(this).ConfigureAwait(false);
+
+            await HighlightCommand.InitializeAsync(this).ConfigureAwait(false);
         }
     }
 
