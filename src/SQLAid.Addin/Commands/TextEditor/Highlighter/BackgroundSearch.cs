197e6fefdb3ae315870b4d620e6a53a707bae195
nb.duong <nb.duong@outlook.com>
2023-01-29 21:51:35 +0700

    Add HighlightCommand

---
+++ src/SQLAid.Addin/Commands/TextEditor/Highlighter/BackgroundSearch.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SQLAid.Commands.TextEditor.Highlighter
+{
+    public class BackgroundSearch
+    {
+        private bool _abort;
+        private static List<SnapshotSpan> s_emptyList = new List<SnapshotSpan>(0);
+        private IList<SnapshotSpan> _matches = s_emptyList;
+        public readonly ITextSnapshot Snapshot;
+
+        public IList<SnapshotSpan> Matches => _matches;
+
+        public BackgroundSearch(ITextSnapshot snapshot, string searchText, bool matchWholeWord, Action completionCallback)
+        {
+            this.Snapshot = snapshot;
+
+            ThreadPool.QueueUserWorkItem(delegate (object state)
+            {
+                System.Threading.Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+                System.Threading.Thread.CurrentThread.IsBackground = true;
+
+                var newMatches = new List<SnapshotSpan>();
+
+                var start = 0;
+                while (true)
+                {
+                    var end = Math.Min(snapshot.Length, start + 4096);
+                    var text = snapshot.GetText(start, end - start);
+
+                    var offset = (start == 0) ? 0 : 1;
+                    while (true)
+                    {
+                        var match = text.IndexOf(searchText, offset, StringComparison.Ordinal);
+                        if (match == -1)
+                            break;
+
+                        if (matchWholeWord)
+                        {
+                            if ((match == 0) || !BackgroundSearch.IsWordCharacter(text[match - 1]))
+                            {
+                                if ((match + searchText.Length == text.Length)
+                                    ? (end == snapshot.Length)
+                                    : !BackgroundSearch.IsWordCharacter(text[match + searchText.Length]))
+                                {
+                                    var matchSpan = new SnapshotSpan(snapshot, match + start, searchText.Length);
+                                    newMatches.Add(matchSpan);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            var matchSpan = new SnapshotSpan(snapshot, match + start, searchText.Length);
+                            newMatches.Add(matchSpan);
+                        }
+
+                        offset = match + searchText.Length;
+                    }
+
+                    if (_abort)
+                        return;
+
+                    if (end == snapshot.Length)
+                        break;
+
+                    start = end - (searchText.Length + 1);
+                }
+
+                _matches = newMatches;
+
+                //completionCallback();
+            });
+        }
+
+        public static bool IsWordCharacter(char c)
+        {
+            return (c == '_') | char.IsLetterOrDigit(c);
+        }
+    }
+}
--- src/SQLAid.Addin/Commands/TextEditor/Highlighter/HighlightCommand.cs
+++ src/SQLAid.Addin/Commands/TextEditor/Highlighter/HighlightCommand.cs
@@ -1,8 +1,14 @@
 ﻿using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
 using Microsoft.VisualStudio.TextManager.Interop;
+using SQLAid.Addin.Logging;
+using SQLAid.Integration;
 using SQLAid.Integration.DTE;
 using System;
 using Task = System.Threading.Tasks.Task;
@@ -13,6 +19,13 @@ namespace SQLAid.Commands.TextEditor.Highlighter
     {
         private static WindowEvents _windowEvents;
         private static IServiceProvider _serviceProvider;
+        private static IWpfTextView _textView;
+        private static IFrameDocumentView _frameDocumentView;
+
+        static HighlightCommand()
+        {
+            _frameDocumentView = new FrameDocumentView();
+        }
 
         public static async Task InitializeAsync(SqlAsyncPackage package)
         {
@@ -29,6 +42,14 @@ namespace SQLAid.Commands.TextEditor.Highlighter
             {
                 var vsTextManager = _serviceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
                 var componentModel = (IComponentModel)_serviceProvider.GetService(typeof(SComponentModel));
+                var editor = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+
+                IVsTextView textViewCurrent;
+                vsTextManager.GetActiveView(1, null, out textViewCurrent);
+                _textView = editor.GetWpfTextView(textViewCurrent);
+
+                _textView.Selection.SelectionChanged += Selection_SelectionChanged;
+
                 Console.WriteLine();
             }
             catch (Exception ex)
@@ -36,8 +57,49 @@ namespace SQLAid.Commands.TextEditor.Highlighter
                 Console.WriteLine();
                 throw;
             }
+        }
 
-            //throw new NotImplementedException();
+        private static void Selection_SelectionChanged(object sender, EventArgs e)
+        {
+            var wpfTextViewLineCollection = _textView.TextViewLines;
+            var textDocument = _frameDocumentView.GetTextDocument();
+            var text = textDocument.CreateEditPoint().GetText(textDocument.EndPoint);
+            UpdateMatches();
+            Console.WriteLine();
+        }
+        private static bool UpdateMatches(bool wereAdornmentsEnabled = true)
+        {
+            var _highlightSpan = SelectionToHighlightSpan();
+
+            var text = _highlightSpan.HasValue ? _highlightSpan.Value.GetText() : null;
+            Logger.Info(text);
+
+            return true;
+        }
+
+        private static SnapshotSpan? SelectionToHighlightSpan()
+        {
+            var selection = _textView.Selection.StreamSelectionSpan.SnapshotSpan;
+
+            if ((selection.Length > 0) && (selection.Length < 128))
+            {
+                if (_textView.Selection.Mode == TextSelectionMode.Box)
+                {
+                    ITextViewLine line = _textView.GetTextViewLineContainingBufferPosition(_textView.Selection.ActivePoint.Position);
+                    if (!line.ContainsBufferPosition(_textView.Selection.AnchorPoint.Position))
+                        return null;
+                }
+
+                int end = selection.End;
+                for (int i = selection.Start; (i < end); ++i)
+                {
+                    var c = selection.Snapshot[i];
+                    if (!char.IsWhiteSpace(c))
+                        return selection;
+                }
+            }
+
+            return null;
         }
     }
 }
