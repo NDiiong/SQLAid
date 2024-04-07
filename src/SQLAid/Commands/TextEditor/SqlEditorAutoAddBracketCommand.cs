96fb38f675b1c02ba3cee6838cdec0e8907a0b3c
N.Duong <nb.duong@outlook.com>
2024-04-07 10:24:36 +0700

    - Add: SqlEditorAutoAddBracketCommand

---
+++ src/SQLAid/Commands/TextEditor/SqlEditorAutoAddBracketCommand.cs
@@ -0,0 +1,54 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using SQLAid.Integration.DTE;
+using Task = System.Threading.Tasks.Task;
+
+namespace SQLAid.Commands.TextEditor
+{
+    internal sealed class SqlEditorAutoAddBracketCommand
+    {
+        private static TextDocumentKeyPressEvents _textDocumentKeyPressEvents;
+
+        static SqlEditorAutoAddBracketCommand()
+        {
+        }
+
+        public static async Task InitializeAsync(SqlAsyncPackage sqlAsyncPackage)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var events = sqlAsyncPackage.Application.Events as Events2;
+            _textDocumentKeyPressEvents = events.TextDocumentKeyPressEvents;
+            _textDocumentKeyPressEvents.BeforeKeyPress += _textDocumentKeyPressEvents_BeforeKeyPress;
+        }
+
+        private static void _textDocumentKeyPressEvents_BeforeKeyPress(string Keypress, TextSelection Selection, bool InStatementCompletion, ref bool CancelKeypress)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (Keypress == "(" || Keypress == "[")
+            {
+                var selectedText = Selection.Text.Trim();
+                if (!string.IsNullOrWhiteSpace(selectedText) && !selectedText.StartsWith("("))
+                {
+                    var result = string.Format("{0}{1}{2}", Keypress, selectedText, ReverseCharacter(Keypress[0]));
+                    Selection.Insert(result);
+                    CancelKeypress = true;
+                }
+            }
+        }
+
+        private static char ReverseCharacter(char input)
+        {
+            switch (input)
+            {
+                case '[': return ']';
+                case ']': return '[';
+                case ')': return '(';
+                case '(': return ')';
+                default:
+                    return input;
+            }
+        }
+    }
+}
