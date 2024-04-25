d75efc172bf5557879c5f56628b4b38ad0443548
N.Duong <nb.duong@outlook.com>
2024-04-25 11:19:08 +0700

    fix: csharp model for command 'SqlResultGridCopyAsCsharpModelCommand'

--- src/SQLAid/Commands/ResultGrid/SqlResultGridCopyAsCsharpModelCommand.cs
+++ src/SQLAid/Commands/ResultGrid/SqlResultGridCopyAsCsharpModelCommand.cs
@@ -7,6 +7,7 @@ using SQLAid.Integration;
 using SQLAid.Integration.Clipboard;
 using SQLAid.Integration.DTE;
 using SQLAid.Integration.DTE.Grid;
+using System;
 using System.CodeDom;
 using System.Data;
 using System.Text;
@@ -45,8 +46,12 @@ namespace SQLAid.Commands.ResultGrid
                 {
                     //Primitive Type
                     var compiler = new CSharpCodeProvider();
-                    var type = new CodeTypeReference(column.DataType);
-                    sb.AppendLine($"    public {compiler.GetTypeOutput(type)} {column.ColumnName} {{ get; set; }}");
+                    if (column.DataType == typeof(DateTime))
+                        sb.AppendLine($"    public DateTime {column.ColumnName} {{ get; set; }}");
+                    else if (column.DataType == typeof(Guid))
+                        sb.AppendLine($"    public Guid {column.ColumnName} {{ get; set; }}");
+                    else
+                        sb.AppendLine($"    public {compiler.GetTypeOutput(new CodeTypeReference(column.DataType))} {column.ColumnName} {{ get; set; }}");
                 }
 
                 sb.AppendLine("}");
