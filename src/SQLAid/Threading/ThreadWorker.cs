09073227faf99af11b49cd6cdc814ce0158a6a8e
N.Duong <nb.duong@outlook.com>
2024-10-19 22:21:21 +0700

    - add ThreadWorker

---
+++ src/SQLAid/Threading/ThreadWorker.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+namespace SQLAid.Threading
+{
+    public class ThreadWorker : BackgroundWorker
+    {
+        private Thread _workerThread;
+
+        protected override void OnDoWork(DoWorkEventArgs e)
+        {
+            _workerThread = Thread.CurrentThread;
+            try
+            {
+                base.OnDoWork(e);
+            }
+            catch (ThreadAbortException)
+            {
+                e.Cancel = true;
+                Thread.ResetAbort();
+            }
+        }
+
+        public void Abort()
+        {
+            if (_workerThread != null && _workerThread.IsAlive)
+            {
+                _workerThread.Abort();
+                _workerThread.Join(500);
+                _workerThread = null;
+            }
+        }
+
+        //private ThreadWorker _worker;
+
+        //private void TryOpenConnection()
+        //{
+        //    _worker = new ThreadWorker();
+        //    _worker.DoWork += OpenConnection;
+        //    _worker.RunWorkerCompleted += CheckConnection;
+        //    _worker.RunWorkerAsync();
+        //}
+
+        //private void CancelConnection()
+        //{
+        //    if (_worker != null && _worker.IsBusy)
+        //    {
+        //        _worker.RunWorkerCompleted -= CheckConnection;
+        //        _worker.Abort();
+        //        _worker.Dispose();
+        //        UpdateControlUsage(true);
+        //        boxServer.Focus();
+        //    }
+        //    else
+        //    {
+        //        DialogResult = DialogResult.Cancel;
+        //    }
+        //}
+    }
+}
